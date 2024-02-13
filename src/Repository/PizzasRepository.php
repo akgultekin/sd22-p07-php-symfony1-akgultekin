715141350a4ed0086f5f0136cc83f210b80b1b18
akgultekin <302188245@student.rocmondriaan.nl>
2024-02-13 12:14:16 +0100

    Created Database

---
+++ src/Repository/PizzasRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Pizzas;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Pizzas>
+ *
+ * @method Pizzas|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Pizzas|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Pizzas[]    findAll()
+ * @method Pizzas[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class PizzasRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Pizzas::class);
+    }
+
+//    /**
+//     * @return Pizzas[] Returns an array of Pizzas objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('p')
+//            ->andWhere('p.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('p.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Pizzas
+//    {
+//        return $this->createQueryBuilder('p')
+//            ->andWhere('p.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
