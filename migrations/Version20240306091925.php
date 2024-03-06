155c75576e1351a81241ca4b862c1d76c1455d21
akgultekin <302188245@student.rocmondriaan.nl>
2024-03-06 10:28:10 +0100

    Changes

--- migrations/Version20240306091925.php
+++ migrations/Version20240306091925.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240306082539 extends AbstractMigration
+final class Version20240306091925 extends AbstractMigration
 {
     public function getDescription(): string
     {
